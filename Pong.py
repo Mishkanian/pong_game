af91fa129262db3aa50951d84a6914677f489afd
Michael Mishkanian <77469811+Mishkanian@users.noreply.github.com>
2021-08-03 17:54:39 -0700

    Initial upload
    Black box comes up on the screen.

---
+++ Pong.py
@@ -0,0 +1,14 @@
+"""
+Pong
+"""
+import turtle
+
+wn = turtle.Screen()
+wn.title("Pong by Michael Mishkanian")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+#Main game loop
+while True:
+    wn.update()
