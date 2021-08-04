d41c6729c8e97bb4d6fcdc7a56605bfa3d62b24f
Michael Mishkanian <77469811+Mishkanian@users.noreply.github.com>
2021-08-03 18:05:09 -0700

    Added paddles and ball

--- Pong.py
+++ Pong.py
@@ -9,6 +9,32 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
-#Main game loop
+# Paddle A (Player's paddle)
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1) # make paddle a rectangle
+paddle_a.penup()
+paddle_a.goto(-350, 0) # starting location of paddle on left side of screen
+
+# Paddle B (Computer Paddle)
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0) # starting location of paddle on right side of screen
+
+# Ball
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.penup()
+paddle_a.goto(0, 0) # ball starts in middle of screen
+
+# Main game loop
 while True:
     wn.update()
